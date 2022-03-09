ac1af994d424acfd4500c100b0fbee9a7cfb1fea
Afride Hossain <54279218+AfrideHossain@users.noreply.github.com>
2022-03-09 22:10:55 +0600

    first upload

---
+++ action.php
@@ -0,0 +1,65 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+
+
+require_once('PHPMailer/PHPMailer.php');
+require_once('PHPMailer/Exception.php');
+require_once('PHPMailer/SMTP.php');
+
+function input_tester($data) {
+
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
+$mailer = new PHPMailer(true);
+
+if (isset($_POST["submit"])) {
+    //echo "Your request is being processing...";
+    $name = input_tester($_POST["client_name"]);
+    $mail = htmlspecialchars($_POST["client_mail"]);
+    $subject = htmlspecialchars($_POST["client_subject"]);
+    $message = htmlspecialchars($_POST["client_message"]);
+    $mbody = "<div style='font-size: 1.2em; padding: 5px;'>
+
+            <div style='border-bottom: 1px solid gray;'>
+                <p style='color: red; font-weight: bold; margin-bottom: 20px; padding: 0; font-size: 2.5em; line-height:0;'>Afride</p>
+            </div>
+            <p><b>Client Name:</b> $name</p>
+            <p><b>Client Email:</b> $mail</p>
+            <p><b>Subject:</b> $subject</p>
+            <br>
+            <span style='background: rgba(0,191,239,0.1); font-size: 1.2em; border: 1px solid #00b1dd; margin: 5px 0px; border-radius: 3px; padding: 10px 30px; margin: 10px 0px; font-weight: bold; text-align: center;'>$message
+            </span>
+            <br>
+            <p style='margin-top: 25px;'>Thank You</p>
+        </div>";
+
+        try {
+            /*$mailer->SMTPDebug  = 3;*/
+            $mailer -> isSMTP();
+            $mailer -> Host = 'smtp.gmail.com';
+            $mailer -> SMTPAuth = true;
+            $mailer -> Username = 'maskfreedom093@gmail.com';
+            $mailer -> Password = 'asdqwe@#12';
+            $mailer -> SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+            $mailer -> Port = '587';
+
+            $mailer -> setFrom('maskfreedom093@gmail.com');
+            $mailer -> addAddress('asafridehossain142@gmail.com');
+            $mailer -> isHTML(true);
+            $mailer -> Subject = $subject;
+            $mailer -> Body = $mbody;
+            $mailer -> send();
+            header("Location: index.php");
+        }
+        catch (Exception $e) {
+            echo "An error occured while sending Email.";
+        }
+} else {
+    header("Location: index.php");
+}
+?>
---
+++ index.php
@@ -0,0 +1,410 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css" type="text/css" media="all" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+    <title>Afride's World</title>
+
+</head>
+<body>
+    <div id="loader">
+                    <div class="loader_text">
+                        <em>Please wait for 8 seconds only</em>
+                        <h2>Website is loading...</h2>
+                        <p>
+                            This website is still under development.
+                        </p>
+                        <p>
+                            So some functionality may not work properly.
+                        </p>
+                    </div>
+                </div>
+    <header id="header_sec_desk" class="header">
+        <div class="web_name">
+            <p>
+                Afride
+            </p>
+        </div>
+        <div class="nav_section">
+            <a href="#home">Home</a>
+            <a href="#about">About</a>
+            <a href="#skills">Skill</a>
+            <a href="#portfolio">Portfolio</a>
+            <a href="#contact">Contactme</a>
+        </div>
+        <div class="box-icon">
+            <i id="mob_menu_btn" class='bx bx-category'></i>
+        </div>
+    </header>
+    <section id="mob_menu" class="mob_menu">
+        <div class="menu_row">
+            <a id="menu_link" href="#home"><i class='bx bx-home-alt'></i>Home</a>
+            <a id="menu_link" href="#about"><i class='bx bx-user'></i>About</a>
+            <a id="menu_link" href="#skills"><i class='bx bx-file'></i>Skills</a>
+        </div>
+        <div class="menu_row">
+            <a id="menu_link" href="#portfolio"><i class='bx bx-images'></i>Portfolio</a>
+            <a id="menu_link" href="#contact"><i class='bx bx-send'></i>Contactme</a>
+            <a id="menu_link" href="#"><i class='bx bx-share-alt'></i>Share</a>
+        </div>
+        <div class="menu_row">
+            <i id="menu_close" class="bx bx-x menu_close"></i>
+        </div>
+    </section>
+    <!-- ====================== Home ====================== -->
+    <section id="home" class="prime_body">
+        <div class="imgNsocial">
+            <div class="prime_body_social">
+                <a href="https://www.facebook.com/hossain.afride.7"><i class="fa fa-facebook"></i></a>
+                <a href="https://www.instagram.com/the_afride"><i class="fa fa-instagram"></i></a>
+                <a href="https://github.com/AfrideHossain"><i class="fa fa-github"></i></a>
+            </div>
+            <div class="prime_body_img">
+                <img src="images/afride.png" alt="" />
+            </div>
+        </div>
+        <div class="prime_body_text">
+            <div>
+                <h2>Hi, I'm Afride</h2>
+                <h4>Full stack web developer</h4>
+                <p>
+                    High level experience in web design and development knowledge. Producing quality work.
+                </p>
+                <a class="all_btn" href="#contact">Contact Me <i class='bx bx-send'></i></a>
+            </div>
+        </div>
+        <!-- <div class="prime_body_img">
+                                                                                    <img src="images/img1_1.jpg" alt="" />
+                                                                                </div> -->
+    </section>
+
+    <!-- ====================== About ====================== -->
+    <section id="about" class="about section">
+        <div class="section_title">
+            <h3>About Me</h3>
+            <h5>My Inroduction</h5>
+        </div>
+        <div class="about_container">
+            <div class="about_img">
+                <img src="images/img2_1.jpg" alt="" />
+            </div>
+            <div class="about_texts">
+                <p>
+                    I want to be a developer. That's why I started web development. I will be a web developing hero. That Will be biggest achievement for my career. I am famous at my college for my presentation skill. And I am a free minded person.
+                </p>
+                <div class="nums">
+                    <p>
+                        06+<span>Languages</span>
+                    </p>
+                    <p>
+                        05+<span>Projects</span>
+                    </p>
+                    <p>
+                        02+<span>Experience</span>
+                    </p>
+                </div>
+                <a class="all_btn" href="Resume_Afride.pdf" download>Resume <i class='bx bx-cloud-download'></i></a>
+            </div>
+        </div>
+    </section>
+
+    <!-- ====================== Skills ====================== -->
+    <section id="skills" class="section">
+        <div class="section_title">
+            <h3>Skills</h3>
+            <h5>My technical level</h5>
+        </div>
+        <div class="skill_container">
+            <div class="skill_type">
+                <div class="skill_type_name">
+                    <div class="skill_type_icon">
+                        <i class='bx bx-code-curly'></i>
+                    </div>
+                    <div class="skill_type_text">
+                        <h4>Frontend Developer</h4>
+                        <h5>More than 2 years</h5>
+                    </div>
+                </div>
+                <div class="lang_rank">
+                    <div class="langname">
+                        <h5>HTML</h5>
+                        <p>
+                            90%
+                        </p>
+                    </div>
+                    <div class="prog_border">
+                        <div style="width: 90%" class="fill_prog"></div>
+                    </div>
+                </div>
+                <div class="lang_rank">
+                    <div class="langname">
+                        <h5>CSS</h5>
+                        <p>
+                            80%
+                        </p>
+                    </div>
+                    <div class="prog_border">
+                        <div style="width: 80%" class="fill_prog"></div>
+                    </div>
+                </div>
+                <div class="lang_rank">
+                    <div class="langname">
+                        <h5>JavaScript</h5>
+                        <p>
+                            70%
+                        </p>
+                    </div>
+                    <div class="prog_border">
+                        <div style="width: 70%" class="fill_prog"></div>
+                    </div>
+                </div>
+                <div class="lang_rank">
+                    <div class="langname">
+                        <h5>ReactJS</h5>
+                        <p>
+                            75%
+                        </p>
+                    </div>
+                    <div class="prog_border">
+                        <div style="width: 70%" class="fill_prog"></div>
+                    </div>
+                </div>
+            </div>
+            <div class="skill_type">
+                <div class="skill_type_name">
+                    <div class="skill_type_icon">
+                        <i class='bx bx-server'></i>
+                    </div>
+                    <div class="skill_type_text">
+                        <h4>Backend Developer</h4>
+                        <h5>More than 2 years</h5>
+                    </div>
+                </div>
+                <div class="lang_rank">
+                    <div class="langname">
+                        <h5>Python</h5>
+                        <p>
+                            80%
+                        </p>
+                    </div>
+                    <div class="prog_border">
+                        <div style="width: 80%" class="fill_prog"></div>
+                    </div>
+                </div>
+                <div class="lang_rank">
+                    <div class="langname">
+                        <h5>PHP</h5>
+                        <p>
+                            60%
+                        </p>
+                    </div>
+                    <div class="prog_border">
+                        <div style="width: 60%" class="fill_prog"></div>
+                    </div>
+                </div>
+                <div class="lang_rank">
+                    <div class="langname">
+                        <h5>MySQL</h5>
+                        <p>
+                            70%
+                        </p>
+                    </div>
+                    <div class="prog_border">
+                        <div style="width: 70%" class="fill_prog"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--======================= Portfolio =======================-->
+    <section id="portfolio" class="section">
+        <div class="section_title">
+            <h3>Portfolio</h3>
+            <h5>My recent works</h5>
+        </div>
+        <div class="portfolio_container">
+            <div class="slide">
+                <div class="slide_img">
+                    <img src="images/web.png" alt="" />
+                </div>
+                <div class="slide_texts">
+                    <h3>E-Commerce site</h3>
+                    <p>
+                        a website that allows you to buy and sell tangible goods, digital products or services online.
+                    </p>
+                </div>
+            </div>
+            <div class="slide">
+                <div class="slide_img">
+                    <img src="images/web.png" alt="" />
+                </div>
+                <div class="slide_texts">
+                    <h3>TextUtilities</h3>
+                    <p>
+                        An online easy to use utility that allows you to manipulate your text.
+                    </p>
+                </div>
+            </div>
+            <div class="slide">
+                <div class="slide_img">
+                    <img src="images/web.png" alt="" />
+                </div>
+                <div class="slide_texts">
+                    <h3>Simple Portfolio</h3>
+                    <p>
+                        a website that allows you to buy and sell tangible goods, digital products or services online.
+                    </p>
+                </div>
+            </div>
+            <div class="arrows">
+                <span onclick="change_slide(1)" class="dot_btn active_circle"></span>
+                <span onclick="change_slide(2)" class="dot_btn"></span>
+                <span onclick="change_slide(3)" class="dot_btn"></span>
+            </div>
+        </div>
+    </section>
+
+    <!--======================= Portfolio =======================-->
+
+    <section id="contact" class="section">
+        <div class="section_title">
+            <h3>Contact Me</h3>
+            <h5>Get in touch</h5>
+        </div>
+        <div class="contact_container">
+            <div class="address">
+                <div class="address_div">
+                    <div class="address_icon">
+                        <i class='bx bx-envelope'></i>
+                    </div>
+                    <div class="address_texts">
+                        <h4>Email</h4>
+                        <p>
+                            asafridehossain142@gmail.com
+                        </p>
+                    </div>
+                </div>
+                <div class="address_div">
+                    <div class="address_icon">
+                        <i class='bx bx-map'></i>
+                    </div>
+                    <div class="address_texts">
+                        <h4>Location</h4>
+                        <p>
+                            Dhaka, Bangladesh
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <!-- ========== Form Here ========== -->
+            <div class="contact_form">
+                <form class="form-body" action="http://firstportfolioafride.000webhostapp.com/afride/action.php" method="post">
+                    <div class="nameNmail">
+                        <div class="input_section size">
+                            <p>
+                                Name
+                            </p>
+                            <input type="text" name="client_name" id="client_name" required/>
+                        </div>
+                        <div class="input_section size">
+                            <p>
+                                Email
+                            </p>
+                            <input type="email" name="client_mail" id="client_mail" required/>
+                        </div>
+                    </div>
+                    <div class="input_section">
+                        <p>
+                            Project/Subject
+                        </p>
+                        <input type="text" name="client_subject" id="client_subject" required/>
+                    </div>
+                    <div class="input_section">
+                        <p>
+                            Message
+                        </p>
+                        <textarea name="client_message" id=client_message rows="8" required></textarea>
+                    </div>
+                    <input class="all_btn" type="submit" name="submit" id="submit" value="Send" />
+                </form>
+            </div>
+        </div>
+    </section>
+
+    <!--======================= Footer =======================-->
+    <footer class="footer">
+        <div class="footer_main">
+            <div class="footer_title">
+                <h3>Afride</h3>
+                <h5>Full stack developer</h5>
+            </div>
+            <div class="footer_nav">
+                <a href="#about">About</a>
+                <a href="#portfolio">Portfolio</a>
+                <a href="#contact">Contactme</a>
+            </div>
+            <div class="footer_social">
+                <a href="https://www.facebook.com/hossain.afride.7"><i class="fa fa-facebook"></i></a>
+                <a href="https://www.instagram.com/the_afride"><i class="fa fa-instagram"></i></a>
+                <a href="https://github.com/AfrideHossain"><i class="fa fa-github"></i></a>
+            </div>
+        </div>
+        <div class="copyright">
+            <p>
+               <i class='bx bx-copyright'></i> Afride. All right reserved.
+            </p>
+        </div>
+    </footer>
+
+    <!--======================= Section for script =======================-->
+
+    <script>
+        /*========================= Loader ============================*/
+        var loader = document.getElementById("loader");
+        setTimeout(function() {
+            loader.style.display = "none";
+        }, 8000);
+        /* ============= Header section Start ============ */
+
+        const header_sec_desk = document.getElementById("header_sec_desk");
+        const mob_menu_btn = document.getElementById("mob_menu_btn");
+        const mob_menu = document.getElementById("mob_menu");
+        const menu_close = document.getElementById("menu_close");
+        const menu_link = document.querySelectorAll("#menu_close");
+        menu_close.onclick = function () {
+            mob_menu.style.bottom = "-100%";
+            header_sec_desk.style.display = "flex"
+        }
+        mob_menu_btn.onclick = function () {
+            mob_menu.style.bottom = "0";
+            header_sec_desk.style.display = "none"
+        }
+
+        /* ============= Slider section Start ============ */
+        var slide_index = 1;
+        show_slide(slide_index);
+        function change_slide(n) {
+            show_slide(slide_index = n);
+        }
+        function show_slide(e) {
+            let i;
+            const slides = document.getElementsByClassName("slide");
+            const dot_btn = document.getElementsByClassName("dot_btn");
+            for (i = 0; i < slides.length; i++) {
+                slides[i].style.display = "none";
+                slides[i].classList.remove("fade");
+                dot_btn[i].classList.remove("active_circle");
+            }
+            slides[slide_index -1].style.display = "flex";
+            slides[slide_index -1].classList.add("fade");
+            dot_btn[slide_index -1].classList.add("active_circle");
+            //setTimeout(show_slide, 2000);
+        }
+        setTimeout(show_slide, 2000);
+    </script>
+
+</body>
+</html>
